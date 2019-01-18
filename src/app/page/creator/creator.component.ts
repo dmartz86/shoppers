67280bb538f5499b49a374bc642022f61212bed3
Daniel Martinez <daniel@monoapps.co>
2019-01-17 20:27:28 -0500

    ADD Budget calculator

--- src/app/page/creator/creator.component.ts
+++ src/app/page/creator/creator.component.ts
@@ -11,6 +11,12 @@ import { CategoriesService } from '../categories/categories.service';
 export class CreatorComponent implements OnInit {
 
   topicName = 'Create';
+  budget = 0;
+  options = [
+    ['1', '4', '7', 'B', { icon: 'chevron_left', digit: 'L' }],
+    ['2', '5', '8', '0', { icon: 'check', digit: 'N' }],
+    ['3', '6', '9', 'C', { icon: 'chevron_right', digit: 'R' }],
+  ];
 
   constructor(
     private router: Router,
@@ -22,6 +28,31 @@ export class CreatorComponent implements OnInit {
       this.router.url.startsWith(item.link));
 
     !topic ? this.router.navigate(['/categories']) : this.topicName = topic.name;
+  }
+
+  command(digit: string) {
+    switch (digit) {
+      case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+        this.budget = this.budget * 10 + parseInt(digit);
+        break;
+      case 'C':
+        this.budget = 0;
+      case 'B':
+        const literal = `${this.budget}`.split('');
+        literal.pop();
+        this.budget = parseInt(`${literal.join('')}`) || 0;
+        break;
+      default: break;
+    }
+  }
+
+  isDisabled(digit: string) {
+    switch (digit) {
+      case 'B': return this.budget < 10;
+      case 'C': return this.budget === 0;
+      default: return `${this.budget}`.length >= 10;
+    }
+
   }
 
 }
